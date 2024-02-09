bb593d8a20de150133c153d74e090a28833d7aea
HotariTobu <hotari24tools@gmail.com>
2024-02-09 14:25:56 +0900

    move hosts

--- extension/vite.config.ts
+++ extension/vite.config.ts
@@ -1,16 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+// import react from "@vitejs/plugin-react-swc";
 import webExtension, { readJsonFile } from "vite-plugin-web-extension";
 import path from "node:path";
 
 const browser = process.env.BROWSER || "chrome"
 
 function generateManifest() {
-  const manifest = readJsonFile("src/manifest.json");
-  const pkg = readJsonFile("package.json");
+  const template = readJsonFile("src/manifest.json");
+  const { version } = readJsonFile("package.json");
   return {
-    version: pkg.version,
-    ...manifest,
+    version,
+    ...template,
   };
 }
 
