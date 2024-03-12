d6bc0f065152d23c317cdce23d0e4ea34d07f8ae
HotariTobu <hotari24tools@gmail.com>
2024-03-13 08:07:09 +0900

    temp: 3/13 08:07

--- extension/src/modifiers/home/dock/items/courses/components/year-term-select.tsx
+++ extension/src/modifiers/home/dock/items/courses/components/year-term-select.tsx
@@ -6,6 +6,9 @@ import { cn } from "@/lib/utils";
 import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group"
 import { Cross1Icon, Cross2Icon, DragHandleDots2Icon, PlusIcon } from "@radix-ui/react-icons";
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+import { useState } from "react";
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
 
 const getTermLabel = (term: string) => {
   const label = t(`home_courses_term_${term}`)
@@ -20,13 +23,40 @@ const getTermLabel = (term: string) => {
 const TermItem = (props: {
   term: string
   selected: boolean
+  onSelect: () => void
+  onEdit: (newTerm: string) => void
+  onDelete: () => void
 }) => {
+  const label = getTermLabel(props.term)
+  const [newTerm, setNewTerm] = useState(label)
+
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition
+  } = useSortable({ id: props.term });
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  }
+
   return (
-    <div className="px-2 h-8 gap-1 text-sm font-medium border rounded-md flex items-center">
-      <DragHandleDots2Icon />
-      <div className="whitespace-nowrap">{getTermLabel(props.term)}</div>
-      <Cross2Icon />
-    </div>
+    <div className={cn("px-2 h-8 gap-1 text-transparent hover:text-foreground text-sm font-medium border rounded-md flex items-center transition-colors", props.selected && "bg-primary/50 border-primary")} style={style} ref={setNodeRef}>
+      < DragHandleDots2Icon {...attributes} {...listeners} />
+      <div className="text-foreground whitespace-nowrap">
+        {props.selected ? (
+          <input className="bg-transparent focus:outline-none" value={newTerm} type="text" />
+        ) : (
+          <div onClick={props.onSelect} role="button" tabIndex={0}>
+            {label}
+          </div>
+        )}
+      </div>
+      <Cross2Icon className=" hover:text-destructive" onClick={props.onDelete} role="button" tabIndex={0} />
+    </div >
   )
 }
 
@@ -36,6 +66,8 @@ export const YearTermSelect = (props: {
   term: string;
   setTerm: (term: string) => void;
 }) => {
+  const [terms, setTerms] = useState(Array.from(store.terms).concat('fir', 'その他'))
+
   return (
     <div className="gap-2 flex items-center">
       <Select value={props.year} onValueChange={props.setYear}>
@@ -50,11 +82,20 @@ export const YearTermSelect = (props: {
       </Select>
       <ScrollArea>
         <div className="gap-1 flex items-center">
-          {Array.from(store.terms).concat('fir', 'その他').map(term => (
-            <TermItem term={term} selected={props.term === term} key={term} />
-          ))}
-          <div className="">
-            <PlusIcon/>
+          {terms.map(term => {
+            const handleSelect = () => {
+              props.setTerm(term)
+            }
+
+            const handleEdit = () => { }
+            const handleDelete = () => { }
+
+            return (
+              <TermItem term={term} selected={props.term === term} onSelect={handleSelect} onEdit={handleEdit} onDelete={handleDelete} key={term} />
+            )
+          })}
+          <div className="px-2">
+            <PlusIcon />
           </div>
         </div>
         <ScrollBar orientation="horizontal" />
